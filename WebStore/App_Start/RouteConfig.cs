e08245b8ad77ea0683fe61162f7d912847cd1798
tyrone0501 <37019997+tyrone0501@users.noreply.github.com>
2018-08-27 09:22:48 +0200

    Update RouteConfig.cs

--- WebStore/App_Start/RouteConfig.cs
+++ WebStore/App_Start/RouteConfig.cs
@@ -12,11 +12,17 @@ namespace WebStore
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute("NewRoute", "App/Do{action}",
+                new { controller = "Home" });
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                
+                new { controller = "Home", action = "Index",
+                    id = UrlParameter.Optional });
             );
         }
     }
