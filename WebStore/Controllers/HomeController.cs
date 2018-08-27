996849abf4089232b95d288cbbaae57c42e7b3e8
tyrone0501 <37019997+tyrone0501@users.noreply.github.com>
2018-08-27 09:46:19 +0200

    Create HomeController.cs

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+
+        public ActionResult CustomVariable(string id = "DefaultId") {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "CustomVariable";
+            ViewBag.CustomVariable = id;
+            return View();
+        }
+
+        public ViewResult MyActionMethod() {
+            string myActionUrl = Url.Action("Index", new { id = "MyID" });
+            string myRouteUrl = Url.RouteUrl(new { controller = "Home",
+                action = "Index" });
+            //... do something with URLs...
+            return View();
+        }
+    }
+}
